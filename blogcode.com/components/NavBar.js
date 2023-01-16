2fb82626c53dd8499a08de79a90899760afb6363
Mohsin Ali <81180134+mohsinalizafar@users.noreply.github.com>
2023-01-16 22:26:31 +0500

    Blogcode

---
+++ blogcode.com/components/NavBar.js
@@ -0,0 +1,57 @@
+import { ReactNode } from 'react';
+import Image from 'next/image'
+import {
+  Box,
+  Flex,
+  Avatar,
+  Link,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
+  Text
+} from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+
+import next from 'next';
+
+;
+
+export default function NavBar() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <Image src="/images/logo.png" width={60} height={100}></Image>
+          <Box >
+            <Button margin={2}>Home</Button>
+            <Button margin={2}>Python</Button>
+            <Button margin={2}>Javascript</Button>
+            <Button margin={2}>C++</Button>
+            <Button margin={2}>About us</Button>
+            <Button margin={2}>Contact us</Button>
+          </Box>
+
+          <Flex alignItems={'center'}>
+            <Stack direction={'row'} spacing={7}>
+              <Button onClick={toggleColorMode}>
+                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+              </Button>
+
+           
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
+}
--- blogcode.com/pages/index.js
+++ blogcode.com/pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-
+import styles from '@/styles/Home.module.css'
+import NavBar from '@/components/NavBar'
+import Footer from '@/components/Footer'
+import Body from '@/components/Body'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -14,14 +16,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <h1>Hello world NextJS app</h1>
-         
-        </div>
-
-       
-      </main>
+      <NavBar/>
+  <Body/>
+      <Footer/>
     </>
   )
 }
